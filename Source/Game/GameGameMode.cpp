aea46712f94cc0d1041d679f0fd21e321512d84c
KMarcjanowicz <51883166+KMarcjanowicz@users.noreply.github.com>
2023-04-25 22:52:31 +0100

    fixed project

--- Source/Game/GameGameMode.cpp
+++ Source/Game/GameGameMode.cpp
@@ -6,12 +6,7 @@
 
 AGameGameMode::AGameGameMode()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/BP_GameCharacter2"));
-	if (PlayerPawnBPClass.Class != NULL)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+
 }
 
 void AGameGameMode::BeginPlay()
